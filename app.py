556f61639f98bf0aeec2c859cf53619b7de41de5
ciciamelani <112041504+ciciamelani@users.noreply.github.com>
2022-08-27 20:12:33 +0700

    Add files via upload

---
+++ app.py
@@ -0,0 +1,8 @@
+# Menginput Sisi Kubus
+sisi = float(input('Tulis Sisi Kubus:'))
+
+#Hitung volume kubus
+volume = sisi ** 3
+
+#Menampilkan hasil perhitungan
+print('volume kubus adalah %0.2f'%volume)
